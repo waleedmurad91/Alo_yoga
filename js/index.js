84464a7ba2b0e2fe186a132ab776a91adc61e819
waleedmurad91 <waleedmurad91@gmail.com>
2025-06-02 21:00:15 +0500

    final

--- js/index.js
+++ js/index.js
@@ -106,3 +106,48 @@ function topFunction() {
   document.body.scrollTop =0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+const toggleBtn = document.getElementById('toggleSidebar');
+const navSidebar = document.getElementById('nav-sidebar');
+const crossBtn = document.getElementById('cross');
+toggleBtn.addEventListener('click', () => {
+  navSidebar.style.display="flex";
+});
+
+crossBtn.addEventListener('click', () => {
+  navSidebar.style.display="none";
+});
+
+
+
+
+const menu = document.getElementById("menu-btns");
+const buttons = menu.getElementsByClassName("menuBtn");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    const name = this.getAttribute("name"); 
+    const classDisplay = name + "-menu-items";
+
+    const allMenus = document.querySelectorAll(".navbar-menu-items > div");
+    allMenus.forEach(div => {
+      div.style.display = "none";
+    });
+
+ 
+    if (classDisplay) {
+      const targetMenus = document.getElementById(classDisplay);
+      console.log(targetMenus);
+      if(targetMenus){
+        targetMenus.style.display = "flex";}
+    }
+
+    
+    const current = document.getElementById("active");
+    if (current) current.removeAttribute("id");
+    this.id = "active";
+  });
+}
